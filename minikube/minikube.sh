8380791eee4f2cf101ea430eb90c54828178332f
Bartosz Budzynowski <bartoszbud@gmail.com>
2025-10-13 17:41:38 +0000

    add application manifests and adjust its vaules

--- minikube/minikube.sh
+++ minikube/minikube.sh
@@ -10,7 +10,7 @@ minikube start
 
 # Deploy ArgoCD
 kubectl create ns argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.5.8/manifests/install.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 # Create Ingress for ArgoCD
 minikube addons enable ingress
