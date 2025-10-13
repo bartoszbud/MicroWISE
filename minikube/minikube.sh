9eba6c72c07a33f155438d80ef137f75b4273a54
Bartosz Budzynowski <bartoszbud@gmail.com>
2025-10-13 19:12:42 +0000

    adjustments

--- minikube/minikube.sh
+++ minikube/minikube.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
+set -e 
 # Download minikube
 curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
 
 # Start minikube
-alias kubectl="minikube kubectl --"
 minikube start
 
 # Deploy ArgoCD
-kubectl create ns argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-
-# Create Ingress for ArgoCD
-minikube addons enable ingress
-
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo
+minikube kubectl -- create ns argocd
+minikube kubectl -- apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
