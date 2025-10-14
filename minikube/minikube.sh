68156cb40ac65d5f91459b9b66685760d9a4b6e4
Bartosz Budzynowski <bartoszbud@gmail.com>
2025-10-14 15:04:28 +0000

    otel collector helm

--- minikube/minikube.sh
+++ minikube/minikube.sh
@@ -11,3 +11,5 @@ minikube start
 # Deploy ArgoCD
 minikube kubectl -- create ns argocd
 minikube kubectl -- apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+#kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
