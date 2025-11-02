9317b96868f76d18659aa7ba5a56c8abd60aa7a4
Bartosz Budzynowski <bartoszbud@gmail.com>
2025-11-02 13:36:50 +0000

    adjust otel-collector config

--- minikube/build.sh
+++ minikube/build.sh
@@ -3,5 +3,5 @@
 set -e
 
 echo -e "Building account-service image... $(minikube image build -t account:0.0.1 ./account)\n"
-echo -e "Building auth-service image... $(minikube image build -t auth-service:0.0.7 ./auth-service)\n"
+echo -e "Building auth-service image... $(minikube image build -t auth-service:0.0.8 ./auth-service)\n"
 echo -e "Building frontend... $(minikube image build -t frontend:0.0.7 ./frontend)\n"
